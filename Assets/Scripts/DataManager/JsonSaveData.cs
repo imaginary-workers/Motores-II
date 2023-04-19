0e0378ff2935578afb8227df2655c78fee5e2d4d
GuidoGabrielePeruilh <guidogabrielep@gmail.com>
2023-04-18 22:31:58 -0300

    Creando los archivos de json

---
+++ Assets/Scripts/DataManager/JsonSaveData.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProyectM2
+{
+    public class JsonSaveData : MonoBehaviour
+    {
+
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
