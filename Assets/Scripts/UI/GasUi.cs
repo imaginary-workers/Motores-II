ac2dd0d9a16551254e4d48a1043ad7db492d4403
Matias0193 <109111507+Matias0193@users.noreply.github.com>
2023-04-23 20:27:10 -0300

    Canvas score
    Canvas de coin y gas

---
+++ Assets/Scripts/UI/GasUi.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ProyectM2
+{
+    public class GasUi : MonoBehaviour
+    {
+        public float _maxTime = 10f;
+        public float _currentTime;
+        public Slider _slider;
+
+        void Start()
+        {
+            _currentTime = _maxTime;
+            _slider.maxValue = _maxTime;
+            _slider.value = _currentTime;
+        }
+
+        void Update()
+        {
+            _currentTime -= Time.deltaTime;
+            _slider.value = _currentTime;
+
+            if (_currentTime <= 0)
+            {
+                Debug.Log("Perdisdte");
+            }
+        }
+
+        public void ReloadSlider()
+        {
+            _currentTime = _maxTime;
+            _slider.value = _currentTime;
+        }
+    }
+}
