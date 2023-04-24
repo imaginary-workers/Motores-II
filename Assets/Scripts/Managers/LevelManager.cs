4e54565e5e3bfc84008f39e18351fd4376ed7a75
GuidoGabrielePeruilh <guidogabrielep@gmail.com>
2023-04-23 21:06:17 -0300

    LevelManager
    Un level manager crear y elimina secciones de nivel utilizando interfaz de observador

--- Assets/Scripts/Managers/LevelManager.cs
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -4,55 +4,57 @@ using UnityEngine;
 
 namespace ProyectM2
 {
-    public class LevelManager : MonoBehaviour
+    public class LevelManager : MonoBehaviour, IObserver
     {
-        [SerializeField] GameObject[] _sections;
-        [SerializeField] float _zPos = 50;
-        [SerializeField] bool _creatingSection = false;
+        [SerializeField] Sections[] _sections;
+        List<Sections> _sectionsListInGame;
+        Sections[] _allSectionsInGame;
         [SerializeField] int _currentIndex;
+
 
         private void Start()
         {
             _currentIndex = 0;
+            _sectionsListInGame = new List<Sections>();
             NewSection(_currentIndex);
-
         }
 
         private void Update()
         {
-            if (!_creatingSection)
+            _allSectionsInGame = FindObjectsOfType<Sections>();
+            foreach (Sections section in _allSectionsInGame)
             {
-                _creatingSection = true;
-                //StartCoroutine(GenerateSection());
-            }
-
-            if (Input.GetKey(KeyCode.A))
-            {
-                NewSection(_currentIndex);
+                if (!_sectionsListInGame.Contains(section))
+                {
+                    _sectionsListInGame.Add(section);
+                    section.Suscribe(this);
+                }
             }
         }
-
-        //IEnumerator GenerateSection()
-        //{
-        //    _sectionIndex = Random.Range(0, _sections.Length);
-        //    Instantiate(_sections[_sectionIndex], new Vector3(0, 1.5f, _zPos), Quaternion.identity);
-        //    _zPos += 50f;
-        //    yield return new WaitForSeconds(2);
-        //    _creatingSection = false;
-        //}
 
         void NewSection(int sectionIndex)
         {
-            if (_currentIndex == 0)
-                Instantiate(_sections[sectionIndex], new Vector3(0,0,0), Quaternion.identity);
-            else
-                Instantiate(_sections[sectionIndex], _sections[sectionIndex - 1].transform.Find("Pivot").transform.position, Quaternion.identity);
-            _currentIndex += 1;
+            if (_currentIndex < _sections.Length)
+            {
+                if (_currentIndex == 0)
+                    Instantiate(_sections[sectionIndex], new Vector3(0,0,0), Quaternion.identity);
+                else
+                    Instantiate(_sections[sectionIndex], _sectionsListInGame[sectionIndex-1].transform.Find("CreateSectionPivot").position, Quaternion.identity);
+                _currentIndex++;
+            }
         }
 
-        void DeleteSection(int sectionIndex)
+
+        public void Notify(string action)
         {
-        }
+            if (action == "Create Section")
+            {
+                
+                _sections[_currentIndex - 1].Unsuscribe(this);
+                NewSection(_currentIndex);
+                _sectionsListInGame.Remove(_sections[_currentIndex - 1]);
+            }
 
+        }
     }
 }
