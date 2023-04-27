e2ab8fdbcf0e29790d07c2a5fb93ffb50b2760f8
GuidoGabrielePeruilh <guidogabrielep@gmail.com>
2023-04-26 21:50:25 -0300

    Update UIGameData.cs

--- Assets/Scripts/UI/UIGameData.cs
+++ Assets/Scripts/UI/UIGameData.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using ProyectM2.Persistence;
 
 namespace ProyectM2
 {
@@ -10,12 +11,18 @@ namespace ProyectM2
         [SerializeField] private TextMeshProUGUI _currencyText;
         [SerializeField] private TextMeshProUGUI _totalCurrencyGainText;
         ValuesToSaveInJson _myJsonData;
+        private DataPersistance _myGameData;
+
+        private void Awake()
+        {
+            _myGameData = GetComponent<DataPersistance>();
+        }
 
         public void GetCurrencyData()
         {
+            _myGameData.LoadGame();
             _currencyText.text += _myJsonData.totalCurrencyOfPlayer;
             _totalCurrencyGainText.text += _myJsonData.totalCurrencyGainOfPlayer;
-
         }
     }
 }
