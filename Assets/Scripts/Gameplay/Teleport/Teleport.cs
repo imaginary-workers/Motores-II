8c80e83f1f832dc988ad80c7366df65449dd7eba
GuidoGabrielePeruilh <guidogabrielep@gmail.com>
2023-05-01 15:20:26 -0300

    Teleport y SessionGameData

--- Assets/Scripts/Gameplay/Teleport/Teleport.cs
+++ Assets/Scripts/Gameplay/Teleport/Teleport.cs
@@ -1,6 +1,7 @@
 using ProyectM2.Managers;
 using UnityEngine;
 using ProyectM2.Persistence;
+using System;
 
 namespace ProyectM2.Gameplay.Teleport
 {
@@ -15,10 +16,33 @@ namespace ProyectM2.Gameplay.Teleport
             if(SessionGameData.GetData("TeleportWasUsed") !=null)
                 _teleportWasUsed = (bool)SessionGameData.GetData("TeleportWasUsed");
 
+            if (SessionGameData.GetData("IsInBonusLevel") != null)
+                _isInBonusLevel = (bool)SessionGameData.GetData("IsInBonusLevel");
+
             if (_teleportWasUsed && !_isInBonusLevel)
                 gameObject.SetActive(false);
 
         }
+
+        private void OnEnable()
+        {
+            EventManager.StartListening("TeleportToBonusLevel", TeleportToLevel);
+            EventManager.StartListening("TeleportReturnToLevel", TeleportToLevel);
+        }
+
+        private void TeleportToLevel(object[] obj)
+        {
+            Debug.Log(obj[0]);
+            Debug.Log((Scene)obj[0]);
+            SceneManager.Instance.ChangeScene((Scene)obj[0]);
+        }
+
+        private void OnDisable()
+        {
+            EventManager.StopListening("TeleportToBonusLevel", TeleportToLevel);
+            EventManager.StopListening("TeleportReturnToLevel", TeleportToLevel);
+        }
+
         private void OnTriggerEnter(Collider other)
         {
             if (other.CompareTag("Player"))
@@ -27,9 +51,14 @@ namespace ProyectM2.Gameplay.Teleport
                 {
                     SessionGameData.SaveData("TeleportWasUsed", true);
                     EventManager.TriggerEvent("TeleportToBonusLevel", _scene);
+                    Debug.Log("Trigger");
                 }
                 else
+                {
                     EventManager.TriggerEvent("TeleportReturnToLevel", _scene);
+                }
+
+                SessionGameData.SaveData("IsInBonusLevel", !_isInBonusLevel);
             }
         }
     }
