71470ddc6b8a586e8c2afe00808a5c88b302a05b
Gabriel Rodriguez <gabriel.drc25.2@gmail.com>
2023-05-31 18:32:15 -0300

    GR
    Se agrego un componente para ejecutar eventos cuando inicia y cuando se vuelve un objeto visible

---
+++ Assets/Scripts/Gameplay/VisibilityEvents.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace ProyectM2.Gameplay
+{
+    public class VisibilityEvents : MonoBehaviour
+    {
+        public UnityEvent OnStart;
+        public UnityEvent OnObjectBecameVisible;
+
+        private void OnBecameVisible()
+        {
+            Debug.Log("Debio RENAUDAR");
+            OnObjectBecameVisible?.Invoke();
+        }
+
+        private void Start()
+        {
+            Debug.Log("Debio DETENER");
+            OnStart?.Invoke();
+        }
+    }
+}
