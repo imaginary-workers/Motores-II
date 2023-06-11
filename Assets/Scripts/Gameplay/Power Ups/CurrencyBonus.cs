cb91b241f66be473700367db96ea57e2ab6e038d
Zebado <sbasualdo.antares@gmail.com>
2023-06-11 17:45:13 -0300

    SB_PowerUp

--- Assets/Scripts/Gameplay/Power Ups/CurrencyBonus.cs
+++ Assets/Scripts/Gameplay/Power Ups/CurrencyBonus.cs
@@ -8,30 +8,16 @@ namespace ProyectM2
 {
     public class CurrencyBonus : MonoBehaviour
     {
-        bool bonus;
+        bool bonus { get; }
         DataInt data;
-        float _time;
-        float _timeMax;
 
         void Update()
         {
             if (bonus)
             {
-                _time += Time.deltaTime;
                 data.value = 2;
-                if(_time > _timeMax) bonus = false;
-                _time = 0;
             }
             else data.value = 1;
-        }
-
-        private void OnTriggerEnter(Collider other)
-        {
-            if (other.CompareTag("Player"))
-            {
-                bonus = true;
-                Destroy(gameObject);
-            }
         }
     }
 }
