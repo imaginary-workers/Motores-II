ed3f14fab407333bb873527b4f930a790f55ee6d
Gabriel Rodriguez <gabriel.drc25.2@gmail.com>
2023-06-13 21:38:00 -0300

    GR Item y Section
    Se agrego los componentes para los itemcard, floatingwindow y section de la ui de store

---
+++ Assets/Scripts/SO/StoreItemSO.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace ProyectM2.SO
+{
+    [CreateAssetMenu(fileName = "StoreItemSO", menuName = "SO/Store Item", order = 0)]
+    public class StoreItemSO : ScriptableObject
+    {
+        public string name;
+        public string description;
+        public Image image;
+        public float price;
+        public string type;
+    }
+}
---
+++ Assets/Scripts/UI/Store/StoreControllerUI.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace ProyectM2.UI.Store
+{
+    public class StoreControllerUI : MonoBehaviour
+    {
+        [SerializeField] private GameObject _sectionPrefab;
+        [SerializeField] private GameObject _itemCardPrefab;
+
+        private void Awake()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
