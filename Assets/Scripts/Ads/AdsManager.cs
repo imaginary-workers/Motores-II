0e526eafc4dd130b63da5693d39d1f95396d97eb
Gabriel Rodriguez <gabriel.drc25.2@gmail.com>
2023-06-16 16:37:43 -0300

    Script test

---
+++ Assets/Scripts/Ads/AdsManager.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using UnityEngine.Advertisements;
+
+namespace ProyectM2.Ads
+{
+    public class AdsManager : MonoBehaviour, IUnityAdsLoadListener, IUnityAdsShowListener, IUnityAdsInitializationListener
+    {
+        [SerializeField] string _androidAdUnitId = "Rewarded_Android";
+        [SerializeField] string _iOSAdUnitId = "Rewarded_iOS";
+        string _adUnitId = null; // This will remain null for unsupported platforms
+
+        private void Awake()
+        {
+#if UNITY_IOS
+            _adUnitId = _iOSAdUnitId;
+#elif UNITY_ANDROID
+            _adUnitId = _androidAdUnitId;
+#elif UNITY_EDITOR
+            _adUnitId = _androidAdUnitId;
+#endif
+            if (!Advertisement.isInitialized && Advertisement.isSupported)
+            {
+                Advertisement.Initialize(_adUnitId, true, this);
+            }
+        }
+
+        [ContextMenu("Load Ad")]
+        public void LoadAd()
+        {
+            Debug.Log("Loading Ad: " + _adUnitId);
+            Advertisement.Load(_adUnitId, this);
+        }
+
+        [ContextMenu("Show Ad")] 
+        public void ShowAd()
+        {
+            Advertisement.Show(_adUnitId, this);
+        }
+
+        public void OnUnityAdsAdLoaded(string adUnitId)
+        {
+            Debug.Log("Ad Loaded: " + adUnitId);
+        }
+
+        public void OnUnityAdsShowComplete(string adUnitId, UnityAdsShowCompletionState showCompletionState)
+        {
+            if (adUnitId.Equals(_adUnitId) && showCompletionState.Equals(UnityAdsShowCompletionState.COMPLETED))
+            {
+                Debug.Log("Unity Ads Rewarded Ad Completed");
+                // Grant a reward.
+            }
+        }
+
+        public void OnUnityAdsFailedToLoad(string adUnitId, UnityAdsLoadError error, string message)
+        {
+            Debug.Log($"Error loading Ad Unit {adUnitId}: {error.ToString()} - {message}");
+            // Use the error details to determine whether to try to load another ad.
+        }
+
+        public void OnUnityAdsShowFailure(string adUnitId, UnityAdsShowError error, string message)
+        {
+            Debug.Log($"Error showing Ad Unit {adUnitId}: {error.ToString()} - {message}");
+            // Use the error details to determine whether to try to load another ad.
+        }
+
+        public void OnUnityAdsShowStart(string adUnitId)
+        {
+        }
+
+        public void OnUnityAdsShowClick(string adUnitId)
+        {
+        }
+
+        public void OnInitializationComplete()
+        {
+            Debug.Log($"Completed initializating Ad Services");
+        }
+
+        public void OnInitializationFailed(UnityAdsInitializationError error, string message)
+        {
+            Debug.Log($"Error initializating Ad Services: {error.ToString()} - {message}");
+        }
+    }
+}
