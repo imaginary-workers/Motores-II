207cd51f4ea6f7f0ac2d2dda22c6ce397b662f4f
GuidoGabrielePeruilh <guidogabrielep@gmail.com>
2023-06-17 18:14:57 -0300

    Update PowerUpsUI.cs

--- Assets/Scripts/UI/PowerUps/PowerUpsUI.cs
+++ Assets/Scripts/UI/PowerUps/PowerUpsUI.cs
@@ -1,21 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 namespace ProyectM2
 {
     public class PowerUpsUI : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
+        [SerializeField] private string _nameOfItemPowerUp;
+        [SerializeField] private TextMeshProUGUI _myText;
+        private int _itemCount;
+        private bool _canUsePowerUp;
+
+        private void Start()
         {
-        
+            _canUsePowerUp = true;
+            var itemsInInventory = ItemProvider.FindSavedItems();
+            if (itemsInInventory.TryGetValue(_nameOfItemPowerUp, out int itemCount))
+            {
+                _itemCount = itemCount;
+                _myText.text = _itemCount.ToString();
+            }
+            else
+                _myText.text = "0";
         }
 
-        // Update is called once per frame
-        void Update()
+        private void SetActivationOfPowerUp()
         {
-        
+            if (_canUsePowerUp)
+            {
+                _myText.color = Color.green;
+                _myText.text = (_itemCount - 1).ToString();
+                _canUsePowerUp = false;
+            }
         }
+
     }
 }
