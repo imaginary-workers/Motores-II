071d4c3b909e69698737a2213c3c0ecad6c480c3
Gabriel Rodriguez <gabriel.drc25.2@gmail.com>
2023-06-24 11:51:34 -0300

    Quitar el que se quite la musica en pause

--- Assets/Scripts/Music/LevelMusicController.cs
+++ Assets/Scripts/Music/LevelMusicController.cs
@@ -1,5 +1,4 @@
 ﻿using ProyectM2.Gameplay;
-using ProyectM2.Persistence;
 using UnityEngine;
 
 namespace ProyectM2.Music
@@ -7,33 +6,7 @@ namespace ProyectM2.Music
     public class LevelMusicController: MonoBehaviour, IActivatable
     {
         [SerializeField] private AudioClip _initMusicLevelClip;
-        private bool _isBonusLevel;
         private float _playedTimeLevelMusic = 0f;
-
-        private void Awake()
-        {
-            var isBonusLevel = SessionGameData.GetData("IsInBonusLevel");
-            _isBonusLevel = isBonusLevel != null && (bool) isBonusLevel;
-        }
-
-        private void OnEnable()
-        {
-            ScreenManager.Instance.Subscribe(this);
-            if (_isBonusLevel) return;
-            // EventManager.StartListening("TeleportToBonusLevel", OnGoToBonesHandler);
-        }
-
-        private void OnDisable()
-        {
-            ScreenManager.Instance.Unsubscribe(this);
-            if (_isBonusLevel) return;
-            // EventManager.StopListening("TeleportToBonusLevel", OnGoToBonesHandler);
-        }
-        //
-        // private void OnGoToBonesHandler(object[] obj)
-        // {
-        //     SessionGameData.SaveData("LevelMusicPlayedTime", MusicManager.Instance.GetPlayedTime());
-        // }
 
         public void Activate()
         {
