bbb8505cba90d7fbef4b8963e3672f48dca36496
Gabriel Rodriguez <gabriel.drc25.2@gmail.com>
2023-06-25 20:31:12 -0300

    Libreria de items

--- Assets/Scripts/SO/DataSkinsSO.cs
+++ Assets/Scripts/SO/DataSkinsSO.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace ProyectM2.SO
@@ -5,8 +7,38 @@ namespace ProyectM2.SO
     [CreateAssetMenu(fileName = "DataSkinsSO", menuName = "SO/Skins Conteiner", order = 0)]
     public class DataSkinsSO : ScriptableObject
     {
-        public Material[] materialesChasis;
-        public Color[] colorWheels;
-        public Color[] colorGlass;
+        public List<ItemData<Material>> materialesChassis;
+        public List<ItemData<Color>> colorWheels;
+        public List<ItemData<Color>> colorGlass;
+
+        [ContextMenu("AddNewChassis")]
+        public void AddNewChassis()
+        {
+            if (materialesChassis.Count == null)
+                materialesChassis = new List<ItemData<Material>>();
+            materialesChassis.Add(new ItemData<Material>());
+        }
+
+        [ContextMenu("AddNewWheels")]
+        public void AddNewWheels()
+        {
+            if (colorWheels.Count == null)
+                colorWheels = new List<ItemData<Color>>();
+            colorWheels.Add(new ItemData<Color>());
+        }
+        [ContextMenu("AddNewGlass")]
+        public void AddNewGlass()
+        {
+            if (colorGlass.Count == null)
+                colorGlass = new List<ItemData<Color>>();
+            colorGlass.Add(new ItemData<Color>());
+        }
+    }
+
+    [Serializable]
+    public class ItemData<T>
+    {
+        private string uKey = Guid.NewGuid().ToString();
+        public T iObject;
     }
 }
