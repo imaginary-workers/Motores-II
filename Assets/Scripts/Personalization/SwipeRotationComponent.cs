0b0d1c92e733c7979d324ccaa7714f863a993eb3
Gabriel Rodriguez <gabriel.drc25.2@gmail.com>
2023-06-26 21:21:32 -0300

    Se agrego el componente que rota el carro

--- Assets/Scripts/Personalization/SwipeRotationComponent.cs
+++ Assets/Scripts/Personalization/SwipeRotationComponent.cs
@@ -2,49 +2,35 @@
 
 namespace ProyectM2.Personalization
 {
-    public class SwipeRotationComponent : MonoBehaviour
+    public class SwipeRotationComponent: MonoBehaviour
     {
-        [SerializeField]
-        private float rotationSpeed = 0.5f;
-    
-        [SerializeField]
-        private Camera mainCamera;
-    
+        [SerializeField] private float rotationSpeed = 0.5f;
+        [SerializeField] private Camera mainCamera;
         private bool firstTouchOnObject;
-        private Vector2 initialTouchPosition;
-    
-        void Start()
-        {
-            if (mainCamera == null)
-            {
-                mainCamera = Camera.main;
-            }
-        }
-    
-        void Update()
+        private Vector2 previousTouchPosition;
+
+        private void Update()
         {
             if (Input.touchCount > 0)
             {
-                Touch touch = Input.GetTouch(0);
-    
+                var touch = Input.GetTouch(0);
+
                 if (touch.phase == TouchPhase.Began)
                 {
-                    // Check if the first touch is on this object
                     RaycastHit hit;
-                    Ray ray = mainCamera.ScreenPointToRay(touch.position);
+                    var ray = mainCamera.ScreenPointToRay(touch.position);
                     if (Physics.Raycast(ray, out hit) && hit.collider.gameObject == gameObject)
                     {
                         firstTouchOnObject = true;
-                        initialTouchPosition = touch.position;
+                        previousTouchPosition = touch.position;
                     }
                 }
                 else if (touch.phase == TouchPhase.Moved && firstTouchOnObject)
                 {
-                    // Calculate the swipe direction based on the difference between the current touch position and initial touch position
-                    Vector2 swipeDirection = touch.position - initialTouchPosition;
-    
-                    // Rotate the object around the Y axis based on the swipe direction
+                    var swipeDirection = touch.position - previousTouchPosition;
                     transform.Rotate(0f, -swipeDirection.x * rotationSpeed, 0f);
+
+                    previousTouchPosition = touch.position;
                 }
                 else if (touch.phase == TouchPhase.Ended && firstTouchOnObject)
                 {
