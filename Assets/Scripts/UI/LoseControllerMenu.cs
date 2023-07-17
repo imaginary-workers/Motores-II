f014c655f2b3d8d6f0eada7a1c71c05d8ed86ce3
Gabriel Rodriguez <gabriel.drc25.2@gmail.com>
2023-07-17 18:04:49 -0300

    Se agrego la funcionalidad de ads en el menu de perder.

---
+++ Assets/Scripts/UI/LoseControllerMenu.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using ProyectM2.Ads;
+using ProyectM2.Stamina;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ProyectM2.UI
+{
+    public class LoseControllerMenu: MonoBehaviour
+    {
+        [SerializeField] private Button _restartButton;
+        private void Awake()
+        {
+            _restartButton.interactable = StaminaSystem.Instance.HasEnoughStamina(1);
+        }
+
+        public void RewardStamina()
+        {
+            AdsManager.Instance.ShowRewardedAd();
+        }
+    }
+}
