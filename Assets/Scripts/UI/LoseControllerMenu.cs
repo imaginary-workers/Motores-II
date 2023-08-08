48b14988aff3ee5bbfb315cd12285dd3177e1165
GuidoGabrielePeruilh <guidogabrielep@gmail.com>
2023-08-08 11:29:53 -0300

    WarningScreen Add
    Se agrego el prefab nada mas del warning

--- Assets/Scripts/UI/LoseControllerMenu.cs
+++ Assets/Scripts/UI/LoseControllerMenu.cs
@@ -1,7 +1,7 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using ProyectM2.Ads;
 using ProyectM2.Stamina;
-using TMPro;
+using ProyectM2.UI.Commands;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,6 +10,11 @@ namespace ProyectM2.UI
     public class LoseControllerMenu : MonoBehaviour
     {
         [SerializeField] private Button _restartButton;
+        [SerializeField] private GameObject _warningAdd;
+        [SerializeField] private GameObject _warningRestarLevel;
+        [SerializeField] private GameObject _warningGoToMainMenu;
+        private Stack<ICommand> commandStack = new Stack<ICommand>();
+
 
         public void RewardStamina()
         {
